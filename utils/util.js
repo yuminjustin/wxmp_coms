51d69cd5a602d3aa60ad5fa67dec4c007cbe654d
justin lee <yuminjustin@163.com>
2020-08-12 15:47:42 +0800

    Create util.js

---
+++ utils/util.js
@@ -0,0 +1,55 @@
+const formatTime = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSeconds()
+
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
+
+const formatNumber = n => {
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
+
+const compareVersion = (v1, v2) => {
+  v1 = v1.split('.')
+  v2 = v2.split('.')
+  const len = Math.max(v1.length, v2.length)
+
+  while (v1.length < len) {
+    v1.push('0')
+  }
+  while (v2.length < len) {
+    v2.push('0')
+  }
+
+  for (let i = 0; i < len; i++) {
+    const num1 = parseInt(v1[i])
+    const num2 = parseInt(v2[i])
+
+    if (num1 > num2) {
+      return 1
+    } else if (num1 < num2) {
+      return -1
+    }
+  }
+  return 0
+}
+
+const getParams = (str) => {
+  let _search = str.split("?")[1],
+    data = {};
+  _search.replace(/([^&]*)=([^&]*)/g, function (a, b, c) {
+    data[b] = c;
+  });
+  return data;
+}
+
+module.exports = {
+  formatTime,
+  compareVersion,
+  getParams
+}
